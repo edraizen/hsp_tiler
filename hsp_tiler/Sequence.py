03da1557f35ebfbd3636f869316e327bae8ca2ad
Eli Draizen <edraizen@elis-mbp.dhcp.lbnl.us>
2014-07-22 10:37:40 -0700

    Changed stop codon from _ to *, fixes issue 1

--- hsp_tiler/Sequence.py
+++ hsp_tiler/Sequence.py
@@ -42,8 +42,8 @@ class TranslationMixins:
     'GGA':'G', 'GGC':'G', 'GGG':'G', 'GGT':'G',
     'TCA':'S', 'TCC':'S', 'TCG':'S', 'TCT':'S',
     'TTC':'F', 'TTT':'F', 'TTA':'L', 'TTG':'L',
-    'TAC':'Y', 'TAT':'Y', 'TAA':'_', 'TAG':'_',
-    'TGC':'C', 'TGT':'C', 'TGA':'_', 'TGG':'W'}
+    'TAC':'Y', 'TAT':'Y', 'TAA':'*', 'TAG':'*',
+    'TGC':'C', 'TGT':'C', 'TGA':'*', 'TGG':'W'}
     def codons(self, start=0, stop=None):
         """Convert nucleotide sequence in frame 1 to a protein sequence
 
